ba339bc052ce010b8b26044c10438eafc0ff7161
Gnimacz <30383499+Gnimacz@users.noreply.github.com>
2023-06-05 00:33:20 +0200

    Auto stash before merge of "RazzleFollower" and "origin/RazzleFollower"

--- Assets/Scripts/PlayerFollower/PlayerFollowerNavmesh.cs
+++ Assets/Scripts/PlayerFollower/PlayerFollowerNavmesh.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerFollowerNavmesh : MonoBehaviour
 {
     [SerializeField] private Transform target;
