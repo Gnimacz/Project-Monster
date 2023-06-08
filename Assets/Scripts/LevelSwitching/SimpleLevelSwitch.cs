bbe31ec5c3e5b59c69eb06dd89e502e3781f0d41
Gnimacz <30383499+Gnimacz@users.noreply.github.com>
2023-06-08 16:32:00 +0200

    Added a loading screen

--- Assets/Scripts/LevelSwitching/SimpleLevelSwitch.cs
+++ Assets/Scripts/LevelSwitching/SimpleLevelSwitch.cs
@@ -5,19 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SimpleLevelSwitch : MonoBehaviour
 {
+
+    public string levelToLoad;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            LevelLoadData.levelName = levelToLoad;
+            SceneManager.LoadScene("LoadingScreen");
         }
     }
 
-    private void Update() {
-        if(Input.GetKeyDown(KeyCode.R))
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
             SceneManager.LoadScene(0);
     }
 }
